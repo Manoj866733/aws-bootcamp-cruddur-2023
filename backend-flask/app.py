c4544d3930caf32c97b299244f21fb4e14c7a7b2
Manoj Kumar <manojkumar.r658@gmail.com>
2023-03-08 16:21:52 +0000

    updated code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.Notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -14,24 +13,43 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-# honeycomb ....
+# HoneyComb ---------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+
+# HoneyComb ---------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+# Show this in the logs within the backend-flask app (STDOUT)
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-# honeycomb ....
+# X-RAY ----------
+XRayMiddleware(app, xray_recorder)
+
+# HoneyComb ---------
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
@@ -86,11 +104,6 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
-  return data, 200
-
-@app.route("/api/activities/Notifications", methods=['GET'])
-def data_Notifications():
-  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,15 +1,21 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
-    }
+     }
 
     now = datetime.now(timezone.utc).astimezone()
-
+    dict = {
+       "now": now.isoformat()
+     }
+    subsegment = xray_recorder.begin_subsegment('mock_data')
+    segment.put_metadata('key', dict, 'namespace')
     if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
+     model['errors'] = ['blank_user_handle']
     else:
       now = datetime.now()
       results = [{
@@ -20,4 +26,11 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+    subsegment = xray_recorder.begin_subsegment('mock_data')
+    dict = {
+       "now": now.isoformat(),
+       "results-size": len(model['data'])
+    }
+    segment.put_metadata('key', dict, 'namespace')
+
     return model
