0040db6741f84211695132d4382499a1f900fed1
Manoj Kumar <manojkumar.r658@gmail.com>
2023-03-21 09:14:20 +0000

    lambda func

---
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -0,0 +1,21 @@
+import json
+import psycopg2
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    try:
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
+        conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
