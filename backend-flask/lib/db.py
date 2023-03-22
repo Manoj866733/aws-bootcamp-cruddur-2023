76bdd10a696b782b07e05c81979183abe74fd832
Manoj Kumar <manojkumar.r658@gmail.com>
2023-03-22 17:36:31 +0000

    updated dy python

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,23 +1,23 @@
 from psycopg_pool import ConnectionPool
 import os
-
-def query_wrap_object(template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
-
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql 
-
-def print_psycopg2_exception(err):
+ 
+class Db:
+  def _init_(self):
+    self.init_pool()
+    
+  def init_pool():  
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+  def query_commit():
+    try:
+      conn = self.pool.connection()
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+    except Exception as err:
+      self.print_sql_err(err)
+      #conn.rollbar()
+  def print_psycopg2_exception(err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
 
@@ -34,16 +34,18 @@ def print_psycopg2_exception(err):
     # print the pgcode and pgerror exceptions
     print ("pgerror:", err.pgerror)
     print ("pgcode:", err.pgcode, "\n")
-    
-def query_commit():
-  try:
-    conn = pool.connection()
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
-  except Exception as err:
-    print_sql_err(err)
-    #conn.rollbar()
-
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+  def query_wrap_object(template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+  def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+db = Db()    
